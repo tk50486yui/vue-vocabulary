2c3f7ee509f27409ce3a332e6d2e3bed22c85e43
Yue <tk50486yui@gmail.com>
2023-12-05 17:09:44 +0800

    feat: configure socket.io and channel listening to update words data

---
+++ src/plugins/laravel-echo-setup.ts
@@ -0,0 +1,10 @@
+import Echo from 'laravel-echo'
+import io from 'socket.io-client'
+// @ts-expect-error:  necessary
+window.io = io
+// @ts-expect-error:  necessary
+window.Echo = new Echo({
+  broadcaster: 'socket.io',
+  host: window.location.hostname + ':6001',
+  withCredentials: true
+})
